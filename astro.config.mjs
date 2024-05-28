2197afea3213895085adb3bdb8f44941b163d3d4
Cristina <avicon95@protonmail.com>
2024-05-28 14:54:43 -0500

    Add Google Analytics for Site

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,24 @@ import react from "@astrojs/react";
 
 import vercel from "@astrojs/vercel/serverless";
 
+import mdx from "@astrojs/mdx";
+import partytown from "@astrojs/partytown";
+
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  integrations: [
+    tailwind(),
+    react(),
+    mdx(),
+    sitemap(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
   output: "server",
   adapter: vercel({
     imageService: true,
