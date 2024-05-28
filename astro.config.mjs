4bb5b852c207105d7d6d37cfed9baaa835ddcd0b
Cristina <avicon95@protonmail.com>
2024-05-28 14:16:08 -0500

    Add GitHub deploy workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,9 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
-import vercel from "@astrojs/vercel/serverless";
-
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
   output: "server",
-  adapter: vercel({
-    imageService: true,
-    webAnalytics: { enabled: true },
-  }),
+  site: "https://anbumangastore.me",
 });
