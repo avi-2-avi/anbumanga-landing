ba3e5d060427cd6842c5c099b1462e7038575243
Cristina <avicon95@protonmail.com>
2024-06-03 22:43:16 -0500

    Hubspot chatbot test - end

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,9 @@
-import { resolve } from "node:path";
-
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
 import vercel from "@astrojs/vercel/serverless";
 import partytown from "@astrojs/partytown";
-import { shield } from "@kindspells/astro-shield";
-
-const rootDir = new URL(".", import.meta.url).pathname;
-const modulePath = resolve(rootDir, "src", "generated", "sriHashes.mjs");
 
 // https://astro.build/config
 export default defineConfig({
@@ -19,19 +13,6 @@ export default defineConfig({
     partytown({
       config: {
         forward: ["dataLayer.push"],
-      },
-    }),
-    shield({
-      sri: {
-        hashesModule: modulePath,
-        enableMiddleware: true,
-
-        scriptsAllowListUrls: [
-          "https://js.hs-banner.com/v2/46369129/banner.js",
-          "https://js.hs-analytics.net/analytics/1717468800000/46369129.js",
-          "https://js-na1.hs-scripts.com/46369129.js",
-        ],
-        stylesAllowListUrls: [],
       },
     }),
   ],
