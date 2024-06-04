b9b43e511b37faac70e4d63d6dda3961ea393237
Cristina <avicon95@protonmail.com>
2024-06-03 22:36:09 -0500

    Hubspot chatbot test

--- astro.config.mjs
+++ astro.config.mjs
@@ -22,7 +22,17 @@ export default defineConfig({
       },
     }),
     shield({
-      sri: { hashesModule: modulePath },
+      sri: {
+        hashesModule: modulePath,
+        enableMiddleware: true,
+
+        scriptsAllowListUrls: [
+          "https://js.hs-banner.com/v2/46369129/banner.js",
+          "https://js.hs-analytics.net/analytics/1717468800000/46369129.js",
+          "https://js-na1.hs-scripts.com/46369129.js",
+        ],
+        stylesAllowListUrls: [],
+      },
     }),
   ],
   output: "server",
